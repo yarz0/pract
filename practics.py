2f51381342bc088827ff7fc9091f63d97c01ee06
yarz0 <40396015+yarz0@users.noreply.github.com>
2024-06-11 01:19:19 +1000

    Add files via upload

---
+++ practics.py
@@ -0,0 +1,23 @@
+a = "hello, world"
+print(len(a))
+
+first = 5
+second = 2
+summa = (first + second)
+diff = (first - second)
+print(summa)
+print(diff)
+
+first = 5; second = 2; third = 3;
+mean = (first+second+third)/2
+print(mean)
+
+first_string = "Вторник"
+second_string = "Понедельник"
+print(second_string  + ', ' + first_string)
+
+a = 2; b = 3; c = 5;
+f = (a*b) + (a*c)
+print((f**3)//2)
+
+
